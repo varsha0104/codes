35cce7e4aa45c039342c3e928d5458c2ee66c4f4
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2022-05-14 19:19:40 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -10,6 +10,38 @@ class Solution{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
+    
+    
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        
+    int i = 1, j = 0,max=1,count = 1;   //i = for arrival and j = for departure
+    
+    sort(arr, arr + n);
+    sort(dep, dep + n);
+
+    while (i < n && j < n)
+    {
+        if (arr[i] <= dep[j])
+        {
+            i++;
+            count++;
+            
+            if(count > max)
+                max = count;
+        }
+        else
+        {
+            j++;
+            count--;
+        }
+    }
+    return max;
+    }
+    
+    
+    
+    /*
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
@@ -29,6 +61,7 @@ class Solution{
         }
         return c;
     }
+    */
     
     
     /*
@@ -73,6 +106,58 @@ class Solution{
         }
     }
     return pl_cnt;
+    }
+    */
+    
+    
+    /*
+    static bool customsort(const pair<int,char> &a,const pair<int,char> &b)
+    {
+        if(a.first == b.first) return a.second < b.second;
+        return a.first < b.first;
+    }
+    
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        vector< pair<int,char> > order;
+        
+        //inserting all the values of time(arrival and departure)
+        //in the vector of pairs.
+        for (int i = 0; i < n; i++) {
+    
+            //if the time is of arrival then we keep second value
+            //of pair as 'a' else 'd'.
+            order.push_back(make_pair(arr[i], 'a'));
+            order.push_back(make_pair(dep[i], 'd'));
+        }
+    
+        //using custom sort vector, first according to time and if time is
+        //same for two events, then arrival comes first followed by departure.
+        sort(order.begin(),order.end(),customsort);
+    
+        int result = 1;
+        int plat_needed = 0;
+        
+        //using an iterator on vector of pairs.
+        vector< pair<int,char> >::iterator it = order.begin();
+    
+        for (; it != order.end(); it++) {
+            //if the second value of vector element is 'a' which stands
+            //for arrival then we add 1 to platform needed else we 
+            //subtract 1 from platform needed.
+            if ((*it).second == 'a')
+                plat_needed++;
+            else
+                plat_needed--;
+            
+            //we keep updating the value of result.
+            if (plat_needed>result)
+                result = plat_needed;
+        }
+        //returning the minimum number of platforms required.
+        return result;
     }
     */
     
