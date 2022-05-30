c1d17b95514968e85aacced7e9af403bf9d3d919
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2022-05-30 22:37:15 +0530

    Added solution - LeetHub

--- Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.cpp
+++ Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.cpp
@@ -18,6 +18,19 @@ class Solution{
             arr[i] = m[i+1];
         }
     }
+    
+    /*
+    //Create a frequency array and store the frequency
+        // of all the element
+        int maxi = max(P,N);
+    	vector<int> count(maxi+1,0);
+        for(int i=0;i<N;i++){
+            count[arr[i]]+=1;
+        }
+        for(int i=0;i<N;i++){
+            arr[i] = count[i+1];
+        }
+    */
 };
 
 
