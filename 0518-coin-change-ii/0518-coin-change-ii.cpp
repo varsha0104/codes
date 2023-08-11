8f2945d591547b7000e965b5876d725a01b50671
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2023-08-11 09:57:38 +0530

    Time: 4 ms (97.86%), Space: 7.1 MB (90.15%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+        dp[0]=1;//1 comb: no coins for no amount 
+        for(int c:coins){
+            for(int i=1; i<=amount; i++)
+                if (i-c>=0)
+                    dp[i]+=dp[i-c];
+        }
+        return dp[amount]; 
+    }
+};
