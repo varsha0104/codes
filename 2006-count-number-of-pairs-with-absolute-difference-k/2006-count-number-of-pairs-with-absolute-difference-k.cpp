0a087613e901bf791067e2c989a57acbc659b0df
varshadhanotiya <77069458+varshadhanotiya@users.noreply.github.com>
2023-09-12 09:48:23 +0530

    Time: 8 ms (87.02%), Space: 14.5 MB (8.42%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,21 +1,37 @@
+// class Solution {
+// public:
+//     int countKDifference(vector<int>& nums, int k) {
+//             int n=nums.size();
+//             int maxi=*max_element(nums.begin(),nums.end());
+
+//             vector<int> arr(maxi+1,0);
+            
+//             for(int i=0;i<n;i++)
+//             {
+//                     arr[nums[i]]++;
+//             }
+//             int ans=0;
+//             for(int i=0;i<n;i++)
+//             {
+//                     if(nums[i]+k<maxi+1)
+//                         ans+=arr[nums[i]+k];
+//             }
+//             return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
             int n=nums.size();
-            int maxi=*max_element(nums.begin(),nums.end());
-
-            vector<int> arr(maxi+1,0);
-            
-            for(int i=0;i<n;i++)
-            {
-                    arr[nums[i]]++;
+            int count = 0 ;
+            unordered_map<int,int>m;
+            for(auto x : nums){
+                count += m[x-k]+m[x+k];
+                m[x]++;
             }
-            int ans=0;
-            for(int i=0;i<n;i++)
-            {
-                    if(nums[i]+k<maxi+1)
-                        ans+=arr[nums[i]+k];
-            }
-            return ans;
+            return count;
     }
 };
